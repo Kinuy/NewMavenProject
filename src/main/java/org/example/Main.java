b0831c8f959cd18463361960f904af9f49dd8ac8
Tobias Weber <toby-weber@gmx.de>
2024-10-01 12:50:33 +0200

    IOException included

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 import com.github.lalyos.jfiglet.FigletFont;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String asciiArt = FigletFont.convertOneLine("Hello World");
         System.out.println(asciiArt);
 
