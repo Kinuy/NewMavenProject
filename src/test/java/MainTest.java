d6b73d1e03bf2201f7493b52fcfd939242010d93
Tobias Weber <toby-weber@gmx.de>
2024-10-01 11:49:25 +0200

    include getNumberTest

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void getNumberTest(){
+        //GIVEN
+        int value = 3;
+        //WHEN
+        int result = Main.getNumber(value);
+        //THEN
+        assertEquals(9,result);
+    }
+}
