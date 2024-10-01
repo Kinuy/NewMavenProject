d488fe5ac419ca09b03dcd80875b4bc9ed31f279
Tobias Weber <toby-weber@gmx.de>
2024-10-01 12:16:22 +0200

    include FigletFont

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
+import com.github.lalyos.jfiglet.FigletFont;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String asciiArt = FigletFont.convertOneLine("Hello World");
+        System.out.println(asciiArt);
     }
 
     public static int getNumber(int value){
